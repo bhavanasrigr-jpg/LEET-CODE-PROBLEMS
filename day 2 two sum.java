45fdc522a8c3fc0ea45c4219e5592f3d059c0350
bhavanasrigr-jpg <bhavanasrigr@gmail.com>
2026-07-17 13:06:17 +0530

    Create day 2 two sum.java

---
+++ day 2 two sum.java
@@ -0,0 +1,11 @@
+lass Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (i=0;i<nums.length;i++){
+            for(j=0;j<nums.length;j++){
+                if (num[i]+num[j])==target){
+                    return new int[]{i,j}
+        }
+        return newint[]{}
+        
+    }
+}
