e4b954ab3eecabd9e03bfca79d26b308c6196fda
bhavanasrigr-jpg <bhavanasrigr@gmail.com>
2026-07-24 08:41:04 +0530

    Create day 2108  find first palindrome string in the array.java

---
+++ day 2108  find first palindrome string in the array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (word.equals(new StringBuilder(word).reverse().toString())) {
+                return word;
+            }
+        }
+        return "";
+    }
+}
